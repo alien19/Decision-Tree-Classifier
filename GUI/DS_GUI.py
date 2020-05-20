d17089bc4d92357351fd765b9fab42697e77eba2
alien19 <1601374@eng.asu.edu.eg>
2020-05-20 05:03:07 +0000

    Linking GUI with the Decision Tree algorithm

--- GUI/DS_GUI.py
+++ GUI/DS_GUI.py
@@ -8,50 +8,58 @@ from PyQt5.QtWidgets import*
 from PyQt5.QtGui import*
 from PyQt5.QtCore import*
 import sys
-# import DS_DecisionTree
-# from PyQt5.QtChart import*
-# from serial import*
+sys.path.append('E:\Engineering courses\Data Structures\Project\Decision-Tree-Classifier')
+path = sys.path[-1]
+from Classifier.DS_DecisionTreeNotebook_Mariam import*
+
 
 class Start_Window(QMainWindow):
 
     def __init__(DStart):
         super(Start_Window,DStart).__init__()
-        DStart.left = 2 #position from left of screen
-        DStart.top = 40 #position from top of screen
+        # DStart.left = 2 #position from left of screen
+        # DStart.top = 40 #position from top of screen
         DStart.title = 'Decision Tree Classifier'
-        DStart.width = 2000 #width of window
-        DStart.height = 1000 #height of window
+        # DStart.width = 2000 #width of window
+        # DStart.height = 1000 #height of window
 
-
+        # DStart.setGeometry
         DStart.InitWindow()
         DStart.Design()
         DStart.setObjectNames()
         DStart.btnTrain.clicked.connect(DStart.onTrainClicked)
-
+        DStart.btnPredict.clicked.connect(DStart.onPredictClicked)
     def InitWindow(DStart):
         DStart.setWindowTitle(DStart.title)
-        DStart.setAutoFillBackground(True)
+        # DStart.setAutoFillBackground(True)
 
         p = DStart.palette()
         p.setColor(DStart.backgroundRole(), Qt.white) #OR teleStart.setStyleSheet("background-color: white;")
         DStart.setPalette(p)
 
-        DStart.mainWidget = QWidget()
+        DStart.mainWidget = QWidget(DStart)
         DStart.mainLayout = QHBoxLayout()
 
-        DStart.lay1 = QVBoxLayout()
-        DStart.layTrain = QHBoxLayout()
+        DStart.lay1 = QStackedLayout()
+        DStart.trainBox = QGroupBox("Decision Tree Classifier")
+        DStart.layTrainBox = QVBoxLayout()
+        DStart.layTrain = QFormLayout()
+
+        DStart.predictBox = QGroupBox("Review Prediciton")
         DStart.lblDepth = QLabel("Decision Tree Maximum Depth")
         DStart.depthEdit  = QLineEdit()
         DStart.depthEdit.setPlaceholderText("maximum depth")
         DStart.btnTrain = QPushButton("Train Decision Tree")
+        DStart.btnPredict = QPushButton("predict new example")
+        DStart.fileEdit = QLineEdit()
+        DStart.fileEdit.setPlaceholderText("testFileName.csv")
         DStart.gBox1 = QGroupBox("Decision Tree Classifier")
         DStart.gBox1 .setAlignment(Qt.AlignCenter)
 
         DStart.lay2 = QVBoxLayout()
         DStart.gBox2 = QGroupBox("Decision Tree")
         DStart.gBox2 .setAlignment(Qt.AlignCenter)
-        DStart.graph = QPixmap("example1_graph.png")
+
         DStart.lblGraph = QLabel()
 
 
@@ -59,22 +67,39 @@ class Start_Window(QMainWindow):
 
 
     def Design(DStart):
-        DStart.gBox1.setMaximumWidth(DStart.mainLayout.maximumSize[0]/2)
-        DStart.mainLayout.addWidget(DStart.gBox1)
-        DStart.mainLayout.addWidget(DStart.gBox2)
-        DStart.gBox1.setLayout(DStart.lay1)
+        qtRectangle = DStart.frameGeometry()
+        centerPoint = QDesktopWidget().availableGeometry().center()
+        qtRectangle.moveCenter(centerPoint)
 
-        DStart.lay1.addLayout(DStart.layTrain)
-        DStart.layTrain.addWidget(DStart.lblDepth)
-        DStart.layTrain.addWidget(DStart.depthEdit)
-        DStart.layTrain.addWidget(DStart.btnTrain)
-
-        DStart.gBox2.setLayout(DStart.lay2)
-        DStart.lay2.addWidget(DStart.lblGraph)
-        DStart.mainWidget.setLayout(DStart.mainLayout)
+        # DStart.lay1.setGeometry((DStart.frameGeometry())/2)
+        # DStart.lay2.setGeometry((DStart.frameGeometry())/2)
         DStart.setCentralWidget(DStart.mainWidget)
+        DStart.mainWidget.setLayout(DStart.mainLayout)
+        DStart.mainLayout.addLayout(DStart.lay1)
+        DStart.mainLayout.addWidget(DStart.gBox2)
 
+        DStart.lay1.addWidget(DStart.trainBox)
+        DStart.lay1.addWidget(DStart.predictBox)
+        DStart.lay1.setCurrentIndex(0)
+        DStart.gBox2.setLayout(DStart.lay2)
+
+        DStart.trainBox.setLayout(DStart.layTrainBox)
+        DStart.layTrainBox.addLayout(DStart.layTrain)
+
+        DStart.layTrain.addRow(DStart.lblDepth,DStart.depthEdit)
+
+        dummyLay = QHBoxLayout()
+        dummyLay.addWidget(DStart.btnTrain)
+        dummyLay.setAlignment(Qt.AlignRight)
+        DStart.layTrain.setAlignment(Qt.AlignCenter)
+        DStart.layTrain.setFormAlignment(Qt.AlignCenter)
+        DStart.layTrain.addRow(dummyLay)
+        DStart.layTrain.addWidget(DStart.fileEdit)
+        DStart.layTrain.addWidget(DStart.btnPredict)
+        DStart.lay2.addWidget(DStart.lblGraph)
         # DStart.show()
+
+
     def setObjectNames(DStart):
         DStart.gBox1.setObjectName("TrainBox")
 
@@ -82,7 +107,28 @@ class Start_Window(QMainWindow):
     @pyqtSlot()
     def onTrainClicked(DStart):
         # Call the decision_tree_algorithm train the model and draw the output graph
+        dataFrame = data_init(path+"\Datasets\sample_train.csv")
+        adjustedDataframe = words_count(dataFrame)
+        tree = decision_tree_algorithm(adjustedDataframe,max_depth=DStart.depthEdit.text)
+
+        draw_graph(tree)
+        d.render(format='png')
+        DStart.graph = QPixmap('graph.gv.png')
+        print("Done")
         DStart.lblGraph.setPixmap(DStart.graph)
+
+
+    def onPredictClicked(DStart):
+        # DStart.statusBar().showMessage("Telemetry started")
+        name,_ = QtWidgets.QFileDialog.getOpenFileName(DStart,'Open File')
+        file = open(name,'r')
+
+        with file:
+            # text = file.read()
+            DStart.fileEdit.setText(file.name)
+
+
+
 
 
 if __name__ == '__main__':
